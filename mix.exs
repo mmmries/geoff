a02534d237f24ba25e2be060195c77e447b397b9
hqmq <michael@riesd.com>
2016-11-05 14:54:57 -0600

    depend on roombex

--- mix.exs
+++ mix.exs
@@ -18,16 +18,9 @@ defmodule Geoff.Mixfile do
      mod: {Geoff, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:roombex, "~> 0.0.5"},
+    ]
   end
 end
